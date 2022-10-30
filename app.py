fc085b701684f3a20c36211fffa761aed2b20384
Rogério Silva <rsilva@guardian.com>
2022-10-30 11:55:33 -0300

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from imp import reload
 from importlib import import_module
 import os
 from flask import Flask, render_template, Response
@@ -37,4 +38,4 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', port=5002, threaded=True, debug=True)
